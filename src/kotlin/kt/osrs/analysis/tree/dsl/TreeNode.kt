15f55f377b47293ac54f1ca9a4a9737c129994e6
TimD <timd@siqinc.com>
2019-01-09 11:20:14 -0500

    fixed error

--- src/kotlin/kt/osrs/analysis/tree/dsl/TreeNode.kt
+++ src/kotlin/kt/osrs/analysis/tree/dsl/TreeNode.kt
@@ -6,20 +6,23 @@ class TreeNode(val type: NodeType = NodeType.AbstractNode, val opcode: Int = -1)
     var parent: TreeNode? = null
 
     //Sets the next node equal to the parameter, also ensures the parents are equal
-    infix operator fun plus(node: TreeNode) = {
+    operator fun plus(node: TreeNode): TreeNode {
         next = node
         node.parent = parent
+        return this
     }
 
     //sets the parent node, also adds as child if needed
-    infix operator fun minus(node: TreeNode) = {
+    operator fun minus(node: TreeNode): TreeNode {
         parent = node
         if (!node.children.contains(this)) node.children.add(this)
+        return this
     }
 
     //adds the node to the list of child nodes, also sets parent of node
-    infix operator fun plusAssign(node: TreeNode) {
+    operator fun times(node: TreeNode): TreeNode {
         children.add(node)
         node.parent = this
+        return this
     }
 }
