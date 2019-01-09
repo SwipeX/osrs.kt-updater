a8422ee04c354557c92dc50d8368cdb1aef576c1
TimD <timd@siqinc.com>
2019-01-09 12:48:26 -0500

    Added all the proper overloads

--- src/kotlin/kt/osrs/analysis/tree/dsl/DslTest.kt
+++ src/kotlin/kt/osrs/analysis/tree/dsl/DslTest.kt
@@ -3,7 +3,7 @@ package kt.osrs.analysis.tree.dsl
 class DslTest {
     fun ye() {
         NodeSequence {
-            fmn() + vn {
+            fmn("{Player}") + vn {
                 this[fmn(), vn()]
             } + vn() - vn {
                 fmn()
--- src/kotlin/kt/osrs/analysis/tree/dsl/NodeSequence.kt
+++ src/kotlin/kt/osrs/analysis/tree/dsl/NodeSequence.kt
@@ -11,12 +11,20 @@ class NodeSequence(init: NodeSequence.() -> TreeNode) {
     fun node(opcode: Int = -1) = TreeNode(NodeType.AbstractNode, opcode)
     fun node(opcode: Int = -1, build: TreeNode.() -> Unit) = TreeNode(NodeType.AbstractNode, opcode).apply(build)
     //VariableNode
+    fun vn(number: Int = -1, build: TreeNode.() -> Unit) = TreeNode(NodeType.VariableNode).value(number).apply(build)
+    fun vn(number: Int = -1) = TreeNode(NodeType.VariableNode).value(number)
     fun vn(opcode: Int = -1, number: Int = -1, build: TreeNode.() -> Unit) = TreeNode(NodeType.VariableNode, opcode).value(number).apply(build)
     fun vn(opcode: Int = -1, number: Int = -1) = TreeNode(NodeType.VariableNode, opcode).value(number)
     //Constant
+    fun cn(cst: Any? = null, build: TreeNode.() -> Unit) = TreeNode(NodeType.ConstantNode).value(cst).apply(build)
+    fun cn(cst: Any? = null) = TreeNode(NodeType.ConstantNode).value(cst)
     fun cn(opcode: Int = -1, cst: Any? = null, build: TreeNode.() -> Unit) = TreeNode(NodeType.ConstantNode, opcode).value(cst).apply(build)
     fun cn(opcode: Int = -1, cst: Any? = null) = TreeNode(NodeType.ConstantNode, opcode).value(cst)
     //FieldMemberNode
+    fun fmn(owner: String? = null) = TreeNode(NodeType.FieldMemberNode).owner(owner)
+    fun fmn(owner: String? = null, build: TreeNode.() -> Unit) = TreeNode(NodeType.FieldMemberNode).owner(owner).apply(build)
+    fun fmn(owner: String? = null, desc: String? = null) = TreeNode(NodeType.FieldMemberNode).owner(owner).desc(desc)
+    fun fmn(owner: String? = null, desc: String? = null, build: TreeNode.() -> Unit) = TreeNode(NodeType.FieldMemberNode).owner(owner).desc(desc).apply(build)
     fun fmn(opcode: Int = -1, owner: String? = null) = TreeNode(NodeType.FieldMemberNode, opcode).owner(owner)
     fun fmn(opcode: Int = -1, owner: String? = null, build: TreeNode.() -> Unit) = TreeNode(NodeType.FieldMemberNode, opcode).owner(owner).apply(build)
     fun fmn(opcode: Int = -1, owner: String? = null, desc: String? = null) = TreeNode(NodeType.FieldMemberNode, opcode).owner(owner).desc(desc)
